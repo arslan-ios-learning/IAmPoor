45f0faad89057b7204619a0cbe43453b70b51dfe
Arslan Mushtaq <arslanark@gmail.com>
2023-04-03 22:12:47 +0500

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Arslan Mushtaq on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
